b041a183cb223cf3626e61d5ab5b3c33dd5b0018
d1qkens <d1qkens@ukr.net>
2016-03-21 20:54:32 +0200

    2103

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'spik2');
+define('DB_NAME', 'spik');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Admin123');
+define('DB_PASSWORD', '123456');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
