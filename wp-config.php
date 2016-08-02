7b53924a16129aa630daf4115367cece9fad0e46
konstantin-pr <kean.dev@gmail.com>
2016-08-02 16:39:03 +0200

    spik 17:35

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'spik');
+define('DB_NAME', 'kpryanichnikovdb');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'kpryanichnikovdb');
 
 /** MySQL database password */
-define('DB_PASSWORD', '123456');
+define('DB_PASSWORD', 'kpryanichnikovshpppass');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
