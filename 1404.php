a554ef307c3b000dd1c2420455cdd2df159f3edb
konstantin-pr <kean.dev@gmail.com>
2016-09-23 12:54:24 +0300

    Right htaccess

---
+++ 1404.php
@@ -0,0 +1,7 @@
+<?php
+header("Refresh=4;url=http://travel-slovenia.ml/");
+?>
+<h3 style="color:orange">Страница не найдена</h3>
+<h4 style="color: #666666">Вернуться на<a href="/" style="color:greenyellow">Главную</a></h4>
+</body>
+</html>
